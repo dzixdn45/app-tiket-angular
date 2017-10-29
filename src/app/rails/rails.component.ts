c4c42a37b8b53b8ce429ac374b65682286ab8b20
Didin nur yahya <didinnuryahya@gmail.com>
2017-10-29 17:26:29 +0700

    update

--- src/app/rails/rails.component.ts
+++ src/app/rails/rails.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule} from '@angular/forms';
+import {Rails} from './rails';
 
 @Component({
   selector: 'app-rails',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rails.component.css']
 })
 export class RailsComponent implements OnInit {
-
-  constructor() { }
+  jForm: FormGroup;
+  post: any;
+  rails: Rails;
+  isTable1 = false;
+  isForm2 = true;
+  titleAlert = 'This field is required';
+  constructor(private fb: FormBuilder) {
+    this.jForm = fb.group({
+      'railsId': [null,
+        Validators.compose([Validators.pattern('^[0-9]+$'),
+        Validators.required])],
+      'railsName': [null, Validators.required],
+      'railsCapacity': [null,
+        Validators.compose([Validators.pattern('^[0-9]+$'),
+        Validators.required])],
+      'validate': ''
+    });
+   }
 
   ngOnInit() {
   }
-
+  addPost(post) {
+    this.rails = new Rails();
+    this.rails.railsId = post.railsId;
+    this.rails.railsName = post.railsName;
+    this.rails.railsCapacity = post.railsCapacity;
+    console.log(this.rails);
+    this.isTable1 = true;
+    this.isForm2 = false;
+  }
 }
---
+++ src/app/rails/rails.ts
@@ -0,0 +1,5 @@
+export class Rails{
+  railsId: Number;
+  railsName: String;
+  railsCapacity: Number;
+}
