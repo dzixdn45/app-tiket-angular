2a9b5d7808964592d84fab2566c894f912074a3f
Didin nur yahya <didinnuryahya@gmail.com>
2017-10-29 14:23:40 +0700

    application ui tiket using angular js
    bisa dikembangkan lagi

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { RailsComponent } from './rails/rails.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    RailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path : 'customer',
+          component : CustomerComponent
+        },
+        {
+          path : 'rails',
+          component : RailsComponent
+        }
+      ]
+    )
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
